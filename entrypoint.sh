34821886d3847720c64c992c2b08c1ae72d84b53
rkygogo <107276912+rkygogo@users.noreply.github.com>
2023-01-05 13:30:30 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,12 @@
 
 sed -i "s/uuid/$uuid/g" /etc/xray/config.json
 sed -i "s/uuid/$uuid/g" /etc/nginx/nginx.conf
-#rm -rf /usr/share/nginx/*
-#if [[ -n $www ]]; then
-#wget -c -P /usr/share/nginx "https://github.com/rkygogo/ddddddd/raw/main/3w/html${www}.zip" 
-#unzip -o "/usr/share/nginx/html${www}.zip" -d /usr/share/nginx/html
-#else
-#wget -c -P /usr/share/nginx "https://github.com/rkygogo/ddddddd/raw/main/3w/html1.zip" 
-#unzip -o "/usr/share/nginx/html1.zip" -d /usr/share/nginx/html
-#fi
+rm -rf /usr/share/nginx/*
+if [[ -n $www ]]; then
+wget -c -P /usr/share/nginx "https://github.com/rkygogo/ddddddd/raw/main/3w/html${www}.zip" 
+unzip -o "/usr/share/nginx/html${www}.zip" -d /usr/share/nginx/html
+else
+wget -c -P /usr/share/nginx "https://github.com/rkygogo/ddddddd/raw/main/3w/html1.zip" 
+unzip -o "/usr/share/nginx/html1.zip" -d /usr/share/nginx/html
+fi
 exec "$@"
