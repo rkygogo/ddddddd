5bda273bf639a4ff4a4f93c0bb358ede784eab7e
rkygogo <107276912+rkygogo@users.noreply.github.com>
2023-01-10 09:36:18 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ unzip -d /usr/local/xray /tmp/Xray-linux-64.zip
 
 sed -i "s/uuid/$uuid/g" /etc/xray/config.json
 sed -i "s/uuid/$uuid/g" /etc/nginx/nginx.conf
-rm -rf /usr/share/nginx/*
+# rm -rf /usr/share/nginx/*
 # www=${www:-'1'}
 # wget -c -P /usr/share/nginx "https://github.com/rkygogo/ddddddd/raw/main/3w/html${www}.zip" 
 # unzip -o "/usr/share/nginx/html${www}.zip" -d /usr/share/nginx/html
