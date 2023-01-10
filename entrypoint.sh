319907a9d342b94aac73bd9442da48d449a2c6d0
rkygogo <107276912+rkygogo@users.noreply.github.com>
2023-01-10 10:31:52 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,9 @@
 # Create By ifeng ygkkk
 # wget -q -O /tmp/xray-linux-64.zip https://github.com/XTLS/Xray-core/releases/download/v1.7.0/Xray-linux-64.zip
 
-wget -qO- https://api.github.com/repos/XTLS/Xray-core/releases/latest | grep -o '"https://github.com/XTLS/Xray-core/releases/download/.*/Xray-linux-64.zip"' | xargs wget -qO /tmp/Xray-linux-64.zip
-unzip -d /usr/local/xray /tmp/Xray-linux-64.zip
-
+wget -qO- temp.zip https://api.github.com/repos/XTLS/Xray-core/releases/latest | grep -o '"https://github.com/XTLS/Xray-core/releases/download/.*/Xray-linux-64.zip"' | xargs wget -qO /tmp/Xray-linux-64.zip
+unzip temp.zip xray geoip.dat geosite.dat
+chmod -v 755 xray
 sed -i "s/uuid/$uuid/g" /etc/xray/config.json
 sed -i "s/uuid/$uuid/g" /etc/nginx/nginx.conf
 [ -n "${www}" ] && rm -rf /usr/share/nginx/* && wget -c -P /usr/share/nginx "https://github.com/rkygogo/ddddddd/raw/main/3w/html${www}.zip" && unzip -o "/usr/share/nginx/html${www}.zip" -d /usr/share/nginx/html
