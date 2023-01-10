42164a245019757fa0acc22a12befa0b63f7e571
rkygogo <107276912+rkygogo@users.noreply.github.com>
2023-01-10 10:15:39 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,5 +11,5 @@ sed -i "s/uuid/$uuid/g" /etc/nginx/nginx.conf
 # www=${www:-'1'}
 # wget -c -P /usr/share/nginx "https://github.com/rkygogo/ddddddd/raw/main/3w/html${www}.zip" 
 # unzip -o "/usr/share/nginx/html${www}.zip" -d /usr/share/nginx/html
-[ -n "${www}" ] && wget -c -P /usr/share/nginx "https://github.com/rkygogo/ddddddd/raw/main/3w/html${www}.zip" && unzip -o "/usr/share/nginx/html${www}.zip" -d /usr/share/nginx/html
+[ -n "${www}" ] && rm -rf /usr/share/nginx/* && wget -c -P /usr/share/nginx "https://github.com/rkygogo/ddddddd/raw/main/3w/html${www}.zip" && unzip -o "/usr/share/nginx/html${www}.zip" -d /usr/share/nginx/html
 exec "$@"
